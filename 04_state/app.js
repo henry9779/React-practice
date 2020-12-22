b386cde3db40a159be396bb7cce6d14ce3147dd4
Henry <vinsen1109@gmail.com>
2020-12-22 14:28:25 +0800

    state

--- 04_state/app.js
+++ 04_state/app.js
@@ -4,19 +4,27 @@
   另一個 count 為 1
  */
 
-function Counter(){
-  /* // TODO
-  var [count, setCount] = React.useState(0);
-  return (
+
+function Counter(props){
+  const [count, setCount] = React.useState(props.count);
+
+  const atClick = () => {
+    setCount( count + 1);
+  }
+
+  return(
     <div className="counter">
       <h1>Counter</h1>
       <div className="count">{count}</div>
+      <button onClick={atClick}>+1</button>
     </div>
-  );
-  // */
+  )
 }
 
 ReactDOM.render(
-  <Counter/>,
+  <div>
+    <Counter count={0}/>
+    <Counter count={1}/>
+  </div>,
   document.getElementById('app')
 );
